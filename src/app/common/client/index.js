24b407e204af644b9a70c110a8597031bd73be36
Cristy Gonzales <cristyg2025@gmail.com>
2023-01-08 15:59:12 -0800

    Removed port information

--- src/app/common/client/index.js
+++ src/app/common/client/index.js
@@ -2,7 +2,7 @@ import feathers from "@feathersjs/client";
 import io from "socket.io-client";
 
 const socket = io(
-  `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`
+  `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_HOST}`
 );
 const client = feathers();
 client.configure(feathers.socketio(socket));
